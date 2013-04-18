f5d9829837d1b1095f3f7044593b80d18f9767c3
Mark Miyashita <negativetwelve@gmail.com>
2013-04-18 14:52:42 -0700

    works but with javascript errors

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function isScrolledIntoView(elem) {
+  var docViewFromTop = $(window).scrollTop();
+  var docViewFromBottom = docViewFromTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom >= docViewFromTop) && (elemTop <= docViewFromBottom));
+}
+
+var item = $('#item');
+var ellDescription = $('#ell_description');
+$(window).scroll(function() {
+  if(isScrolledIntoView(item)) {
+    ellDescription.fadeIn('500'); 
+  } else {
+    ellDescription.fadeOut('500')
+  }
+});
+
+$(window).scroll(function(){
+
+  $("#item").css({
+    top: function(index, value) {
+      return 350 - $(window).scrollTop() * 1.3;
+    }
+  });
+});
